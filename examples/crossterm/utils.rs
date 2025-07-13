e4e057b3b50636241cc87b186b231ecbb77fd0cc
rezigned <rezigned@users.noreply.github.com>
2025-07-14 01:08:47 +0700

    Cleanup before v1.0.0 release (#31)
    * docs: rephrase wording in README.md
    
    * chore: add script to test release-please
    
    * chore: move modes.rs example outside examples/crossterm/ dir

--- examples/crossterm/utils.rs
+++ examples/crossterm/utils.rs
@@ -1,11 +1,15 @@
+use std::io;
+
 #[cfg(feature = "crossterm")]
 use crossterm::{cursor, execute, style::Print};
+use crossterm::{
+    event::{read, Event, KeyEvent},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
 
-#[allow(unused)]
+#[allow(dead_code)]
 #[cfg(feature = "crossterm")]
 pub(crate) fn output<T: std::fmt::Display>() -> impl FnMut(T) -> std::io::Result<()> {
-    use std::fmt::Display;
-
     use crossterm::terminal::{Clear, ClearType};
 
     let mut stdout = std::io::stdout();
@@ -17,6 +21,37 @@ pub(crate) fn output<T: std::fmt::Display>() -> impl FnMut(T) -> std::io::Result
             Print(s),
         )
     }
+}
+
+#[allow(dead_code)]
+pub(crate) fn print(s: &str) -> bool {
+    println!("{s}\r");
+    false
+}
+
+#[allow(dead_code)]
+pub(crate) fn quit(s: &str) -> bool {
+    println!("{s}\r");
+    true
+}
+
+#[allow(dead_code)]
+pub(crate) fn run<F>(mut f: F) -> io::Result<()>
+where
+    F: FnMut(KeyEvent) -> bool,
+{
+    enable_raw_mode()?;
+
+    loop {
+        if let Event::Key(key) = read()? {
+            let quit = f(key);
+            if quit {
+                break;
+            }
+        }
+    }
+
+    disable_raw_mode()
 }
 
 #[allow(unused)]
