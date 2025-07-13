737828d5470efe0763f8a054b81bce3b3e41c9cb
Marut Khumtong <me@rezigned.com>
2025-07-14 01:01:55 +0700

    chore: move modes.rs example outside examples/crossterm/ dir

---
+++ examples/modes.rs
@@ -0,0 +1,84 @@
+use std::collections::HashMap;
+
+use keymap::DerivedConfig;
+use serde::Deserialize;
+
+use crate::crossterm_utils::{print, quit, run};
+
+#[path = "./crossterm/utils.rs"]
+mod crossterm_utils;
+
+#[derive(keymap::KeyMap, Deserialize, Debug, Hash, Eq, PartialEq)]
+enum HomeAction {
+    #[key("esc")]
+    Quit,
+    #[key("e")]
+    Edit,
+}
+
+#[derive(keymap::KeyMap, Deserialize, Debug, Hash, Eq, PartialEq)]
+enum EditAction {
+    #[key("esc")]
+    Exit,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+enum Actions {
+    Home(DerivedConfig<HomeAction>),
+    Edit(DerivedConfig<EditAction>),
+}
+
+type Modes = HashMap<String, Actions>;
+
+#[allow(unused)]
+pub(crate) const CONFIG: &str = r#"
+[home]
+Quit = { keys = ["esc", "q"], description = "Quit the app" }
+Edit = { keys = ["e"], description = "Enter edit mode" }
+
+[edit]
+Exit = { keys = ["esc", "q"], description = "Exit edit mode" }
+"#;
+
+fn main() -> std::io::Result<()> {
+    let modes: Modes = toml::from_str(CONFIG).unwrap();
+    let mut mode = "home";
+
+    println!("mode: {mode}\r");
+
+    run(move |key| match mode {
+        "home" => {
+            let Some(Actions::Home(config)) = modes.get(mode) else {
+                return false;
+            };
+
+            match config.get(&key) {
+                Some(action) => match action {
+                    HomeAction::Quit => quit("quit!"),
+                    HomeAction::Edit => {
+                        mode = "edit";
+                        print("enter edit mode!")
+                    }
+                },
+                None => print(&format!("{}", key.code)),
+            }
+        }
+        "edit" => {
+            let Some(Actions::Edit(config)) = modes.get(mode) else {
+                return false;
+            };
+
+            match config.get(&key) {
+                Some(action) => match action {
+                    EditAction::Exit => {
+                        mode = "home";
+                        print("exit edit mode!")
+                    }
+                },
+                None => print(&format!("{}", key.code)),
+            }
+        }
+        _ => unreachable!(),
+    })
+}
